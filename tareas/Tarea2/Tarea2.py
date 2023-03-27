fc3616f28731862109c7fc2267df4fade4996adf
MrFork27 <aitormeleropicon@gmail.com>
2023-03-27 21:28:28 +0200

    Tarea 2 - Ejercicio 1

--- tareas/Tarea2/Tarea2.py
+++ tareas/Tarea2/Tarea2.py
@@ -1,4 +1,11 @@
 # Ejercicio 1
+real_number = float(input("Introduce un número real: "))
+if real_number > 0:
+    print("El número es positivo.")
+elif real_number < 0:
+    print("El número es negativo.")
+else:
+    print("El número es nulo.")
 
 
 # Ejercicio 2
